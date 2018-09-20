f4e293677db5a65d4a1606715f5a2c02151dd5e0
Ebrahim-hamdy <ebrahim0hamdy@gmail.com>
2018-09-21 00:11:32 +0300

    Build tower code

---
+++ src/control/tower.js
@@ -0,0 +1,27 @@
+export default {
+
+	getTowers: () => {
+		return Object.keys(Game.structures)
+			.map(id => Game.structures[id])
+			.filter(structure => structure.structureType === STRUCTURE_TOWER);
+	},
+
+	run: (tower) => {
+		if (!tower) { return; }
+
+		let closestHostile = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
+
+		if (closestHostile) {
+			tower.attack(closestHostile);
+
+		} else {
+			let closestDamagedStructure = tower.pos.findClosestByRange(FIND_STRUCTURES, {
+				filter: (structure) => structure.hits < structure.hitsMax
+			});
+
+			if (closestDamagedStructure) {
+				tower.repair(closestDamagedStructure);
+			}
+		}
+	}
+};
